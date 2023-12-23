cd5dceeafc9aec8f0b4ed1c0dcb6d44e1916e835
--replace-all <nadjsharief@gmail.com>
2023-12-23 22:52:59 +0800

    added navigation bar and main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Nav from './components/navbar/navbarComponent.vue'
+import Main from './components/main.vue'
 </script>
-
 <template>
   <Nav></Nav>
-  <RouterView />
+  <Main></Main>
+  <RouterView/>
 </template>
